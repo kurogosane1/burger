ccf0e103d7f674cb80ae59f823dc4d592459322b
Syed Khurshid <magnite@SyedKhushidsMBP.lan>
2017-02-19 15:58:55 -0600

    Made changes to deply to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connectionInfo = {
   host: "localhost",
   user: "root",
   password: "saadkhurshid310588",
   database: "burger_db"
-});
+};
+
+if (process.env.JAWSDB_URL)
+{
+  connectionInfo = process.env.JAWSDB_URL;
+}
+
+var connection = mysql.createConnection(connectionInfo);
 
 connection.connect(function(err) {
   if (err) {
